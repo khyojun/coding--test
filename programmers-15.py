2e4d63d800800e1fc885de75f705d34fca5a877c
khyojun <38220795+khyojun@users.noreply.github.com>
2022-07-23 23:29:16 +0900

    프로그래머스 level 1 완주하지 못한 선수 - dictionary활용

---
+++ programmers-15.py
@@ -0,0 +1,22 @@
+def solution(participant, completion): # 실패 끝 딕셔너리로 해결
+    answer = ''
+    par_dict={}
+    com_dict={}
+    for i in range(len(participant)):
+        par_dict[participant[i]]=0
+        
+        
+    for i in range(len(participant)):
+        par_dict[participant[i]]=par_dict[participant[i]]+1
+            
+            
+    for i in range(len(completion)):
+        par_dict[completion[i]]-=1
+             
+    for i in range(len(participant)):
+        if par_dict[participant[i]]>=1:
+            return participant[i]
+            
+        
+    
+        
