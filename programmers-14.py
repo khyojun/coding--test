37af7e05299fe32d44f69b963a40132cae0f2521
khyojun <38220795+khyojun@users.noreply.github.com>
2022-07-23 23:28:46 +0900

    프로그래머스 leve1 폰켓몬

---
+++ programmers-14.py
@@ -0,0 +1,22 @@
+def solution(nums): # 폰켓몬
+    answer = 0
+    chosen_length=len(nums)//2
+    tmp=set(nums)
+    num_li=list(tmp)
+    answer_leng=len(num_li)
+    
+    
+    
+    if chosen_length<answer_leng:
+        answer=chosen_length
+    else:
+        answer=answer_leng
+    
+    
+    
+    
+    
+    
+    
+    
+    return answer
