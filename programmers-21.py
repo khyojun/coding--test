badd3f4265737f678a25a1dd021bf5394661cb58
khyojun <38220795+khyojun@users.noreply.github.com>
2022-07-28 20:00:12 +0900

    프로그래머스 level1 3진법 뒤집기

---
+++ programmers-21.py
@@ -0,0 +1,19 @@
+def solution(n):
+    answer = 0
+    three=[]
+    while True:
+        if n<3:
+            three.append(n)
+            break
+        three.append(n%3)
+        n=n//3
+    
+    cnt_three=1
+    leng=len(three)
+    for i in range(len(three)-1, -1, -1):
+        answer+=cnt_three*three[i]
+        cnt_three*=3
+        
+    return answer
+    
+    
